f4c675f506b4491876d365ba03d32b7e05664d20
Rasmus Loqvist <rasmus.loqvist@learnifier.com>
2018-05-18 00:21:27 +0200

    Change secret

--- factory/app_creator.py
+++ factory/app_creator.py
@@ -1,4 +1,4 @@
-from server_config import debug, sqlite_uri
+from server_config import debug, sqlite_uri, secret
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
@@ -9,8 +9,8 @@ def init_app(name):
     app.config['DEBUG'] = debug
     app.config["SQLALCHEMY_DATABASE_URI"] = sqlite_uri
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config["SECRET_KEY"] = "secret"
-    
+    app.config["SECRET_KEY"] = secret
+
     db = SQLAlchemy(app)
 
     return app
